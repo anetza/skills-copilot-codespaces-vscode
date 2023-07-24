f808b60d21b475aab7f6eb68c7305cc1b4a7d3f4
anetza <140365968+anetza@users.noreply.github.com>
2023-07-24 16:42:24 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,55 @@
+// Create web server
+// const http = require("http");
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/") {
+//     res.write("Hello world from nodejs");
+//     res.end();
+//   } else {
+//     res.write("using some other domain");
+//     res.end();
+//   }
+// });
+// server.listen("3000");
+
+// Path: comments.js
+// const _ = require("lodash");
+// const items = [1, [2, [3, [4]]]];
+// const newItems = _.flattenDeep(items);
+// console.log(newItems);
+// console.log("hello world");
+
+// Path: comments.js
+// const fs = require("fs");
+// fs.writeFileSync("notes.txt", "My name is John Doe");
+// fs.appendFileSync("notes.txt", ". I live in Philadelphia");
+
+// Path: comments.js
+// const validator = require("validator");
+// const getNotes = require("./notes.js");
+// const msg = getNotes();
+// console.log(msg);
+// console.log(validator.isURL("https://www.google.com"));
+
+// Path: comments.js
+// const chalk = require("chalk");
+// console.log(chalk.green("Success!"));
+// console.log(chalk.red("Error!"));
+// console.log(chalk.blue("Info!"));
+
+// Path: comments.js
+// const validator = require("validator");
+// const getNotes = require("./notes.js");
+// const msg = getNotes();
+// console.log(msg);
+// console.log(validator.isURL("https://www.google.com"));
+
+// Path: comments.js
+// const chalk = require("chalk");
+// console.log(chalk.green("Success!"));
+// console.log(chalk.red("Error!"));
+// console.log(chalk.blue("Info!"));
+
+// Path: comments.js
+// const fs = require("fs");
+// const book = {
+//   title: "Ego is the Enemy",
